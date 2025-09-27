7a0002ec542ee6990609fd24d06e3d2ba7530045
Antoni <a.wrzesinski1@gmail.com>
2025-09-27 14:48:21 +0200

    Added border on level 1 and changed background color

--- Scripts/diezone.gd
+++ Scripts/diezone.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	
+
 func _on_body_entered(body):
-	body.die()
+	# Only call die() if the body has that method
+	if body.has_method("die"):
+		body.die()
