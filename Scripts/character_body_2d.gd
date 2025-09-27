f1157eed00ef977a547315658185e45e8646e424
Antoni <a.wrzesinski1@gmail.com>
2025-09-27 11:20:32 +0200

    Updated character movment script
    Made character Movement more smooth and made character change sprite image acording to the direction

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -1,11 +1,48 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var grid_size: int = 6
+@export var move_speed: float = 200
 
-func get_input():
-	var input_direction = Input.get_vector("A_Left", "D_Right", "W_Foward", "S_Backwards")
-	velocity = input_direction * speed
+@onready var sprite = $Sprite2D
+
+# Load images from filesystem
+var sprite_up   = load("res://Images/Character/Up.png")
+var sprite_down = load("res://Images/Character/Down.png")
+var sprite_left = load("res://Images/Character/Left.png")
+var sprite_right = load("res://Images/Character/Right.png")
+
+var target_position: Vector2
+var moving: bool = false
+var last_dir: Vector2 = Vector2.ZERO 
+
+func _ready():
+	target_position = position
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if moving:
+		position = position.move_toward(target_position, move_speed * delta)
+		if position == target_position:
+			moving = false
+	else:
+		handle_input()
+
+func handle_input():
+	var dir = Vector2.ZERO
+
+	if Input.is_action_pressed("D_Right"):
+		dir = Vector2.RIGHT
+		sprite.texture = sprite_right
+	elif Input.is_action_pressed("A_Left"):
+		dir = Vector2.LEFT
+		sprite.texture = sprite_left
+	elif Input.is_action_pressed("W_Foward"):
+		dir = Vector2.UP
+		sprite.texture = sprite_up
+	elif Input.is_action_pressed("S_Backwards"):
+		dir = Vector2.DOWN
+		sprite.texture = sprite_down
+
+	if dir != Vector2.ZERO:
+		last_dir = dir
+		target_position = position + dir * grid_size
+		moving = true
