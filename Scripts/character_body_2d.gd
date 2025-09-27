a08b40e0fb0d926809ce139b5339a9cf17ab0219
Antoni <a.wrzesinski1@gmail.com>
2025-09-27 15:58:19 +0200

    fixed player not stopping for collisions

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -13,41 +13,43 @@ var sprite_right = load("res://Images/Character/Right.png")
 
 var target_position: Vector2
 var moving: bool = false
-var last_dir_x: Vector2 = Vector2.ZERO
-var last_dir_y: Vector2 = Vector2.ZERO 
 
 func _ready():
 	target_position = position
 
 func _physics_process(delta):
 	if moving:
-		position = position.move_toward(target_position, move_speed * delta)
+		# Move towards the target tile
+		var step = move_speed * delta
+		position = position.move_toward(target_position, step)
 		if position == target_position:
 			moving = false
 	else:
 		handle_input()
 
 func handle_input():
-	var dir_x = Vector2.ZERO
-	var dir_y = Vector2.ZERO
+	var dir = Vector2.ZERO
+
 	if Input.is_action_pressed("W_Foward"):
-		dir_y = Vector2.UP
+		dir = Vector2.UP
 		sprite.texture = sprite_up
 	elif Input.is_action_pressed("S_Backwards"):
-		dir_y = Vector2.DOWN
+		dir = Vector2.DOWN
 		sprite.texture = sprite_down
-	if Input.is_action_pressed("D_Right"):
-		dir_x = Vector2.RIGHT
-		sprite.texture = sprite_right
 	elif Input.is_action_pressed("A_Left"):
-		dir_x = Vector2.LEFT
+		dir = Vector2.LEFT
 		sprite.texture = sprite_left
+	elif Input.is_action_pressed("D_Right"):
+		dir = Vector2.RIGHT
+		sprite.texture = sprite_right
 
-	if dir_x != Vector2.ZERO or dir_y != Vector2.ZERO:
-		last_dir_x = dir_x
-		last_dir_y = dir_y
-		target_position = position + (dir_x * grid_size) + (dir_y * grid_size)
-		moving = true
+	if dir != Vector2.ZERO:
+		# Check if the next tile is free before moving
+		var motion = dir * grid_size
+		var collision = move_and_collide(motion)
+		if collision == null:
+			target_position = position + motion
+			moving = true
 
 func die():
 	get_tree().call_deferred("reload_current_scene")
