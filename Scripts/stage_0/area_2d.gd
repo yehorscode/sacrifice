7adf9f4c219bbd86c4e7546f439061e134a8357b
Yehor <omelianenkoegor@gmail.com>
2025-09-27 12:31:37 +0200

    e

--- Scripts/stage_0/area_2d.gd
+++ Scripts/stage_0/area_2d.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
 func _on_area_2d_body_entered(body):
-	print("entered")
+	if body.global_position.x < global_position.x:
+		print("z lewej")
+	elif body.position.x > global_position.x:
+		print("z prawej")
+	elif body.position.y > global_position.y:
+		print("z góry")
+
+func _process(delta: float) -> void:
+	
