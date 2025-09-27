0fd563048bf656b8496a1ac94a12f41bcc096eeb
Yehor <omelianenkoegor@gmail.com>
2025-09-27 16:14:37 +0200

    e

--- Scripts/arrow.gd
+++ Scripts/arrow.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
-@export var speed: float = 500.0
+@export var speed: float = 1000.0
 var direction: Vector2 = Vector2.RIGHT
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	body.die()
+	queue_free()
 
 func _process(delta: float) -> void:
 	position += direction * speed * delta
-
-func _on_Area2D_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
-		if body.has_method("die"):
-			body.die()   # Call player's death function
-		queue_free()
--- Scripts/diezone.gd
+++ Scripts/diezone.gd
@@ -4,6 +4,5 @@ func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
-	# Only call die() if the body has that method
 	if body.has_method("die"):
 		body.die()
